5d810a5987fb3320c0e31c902a747d80e73b5689
15zxd <yepeng@insona.cc>
2019-12-20 12:24:37 +0800

    fix: LED返回

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/conthing/utils v0.0.0-20191209094002-60c1a83343b2
 	github.com/gin-gonic/gin v1.5.0
 	github.com/grandcat/zeroconf v0.0.0-20190424104450-85eadb44205c
+	github.com/json-iterator/go v1.1.7
 	github.com/miekg/dns v1.1.25 // indirect
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
 	gopkg.in/yaml.v2 v2.2.7
--- services/checkhealth.go
+++ services/checkhealth.go
@@ -4,6 +4,7 @@ import (
 	"github.com/conthing/utils/common"
 	"github.com/json-iterator/go"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os/exec"
 	"sysmgmt-next/config"
@@ -24,29 +25,38 @@ func LED(s config.MicroService) {
 	resp, err := http.Get(s.URL)
 	// http 连接异常
 	if err != nil {
-		str, _ := exec.Command("/usr/test/led-hrtimer-close", s.Name).Output()
+		str, _ := exec.Command("/usr/test/led-hrtimer-close", s.LED).Output()
 		common.Log.Error("Get url 出错", err, "关灯 -> ", string(str))
+		return
 	}
 	// body 分类
 	data, err := ioutil.ReadAll(resp.Body)
 	common.Log.Info(string(data))
 	// body 异常
 	if err != nil {
-		str, _ := exec.Command("/usr/test/led-hrtimer-close", s.Name).Output()
+		str, _ := exec.Command("/usr/test/led-hrtimer-close", s.LED).Output()
 		common.Log.Error("解析 body 出错", err, "关灯 -> ", string(str))
+		return
 	}
 	// ping
-	if s.Type == "ping" && string(data) == "pong" {
-		str, _ := exec.Command("/usr/test/led-pwm-start-percentage", s.Name, "2", "1").Output()
-		common.Log.Info("Get url pong", "开灯 -> ", string(str))
-	}
-	// status
-	if s.Type == "status" && jsoniter.Get(data, "status").ToString() == "connected" {
-		str, _ := exec.Command("/usr/test/led-pwm-start-percentage", s.Name, "2", "1").Output()
-		common.Log.Info("Get url status connected", "开灯 -> ", string(str))
-
-	} else {
-		str, _ := exec.Command("/usr/test/led-hrtimer-close", s.Name).Output()
-		common.Log.Error("Get url", err, "关灯 -> ", string(str))
+	switch s.Type {
+	case "ping":
+		if string(data) == "pong" {
+			exec.Command("/usr/test/led-pwm-start-percentage", s.LED, "2", "1").Output()
+			common.Log.Info("Get URL pong", "开灯")
+		} else {
+			exec.Command("/usr/test/led-hrtimer-close", s.LED).Output()
+			common.Log.Error("Get not pong", "关灯")
+		}
+	case "status":
+		if jsoniter.Get(data, "status").ToString() == "connected" {
+			exec.Command("/usr/test/led-pwm-start-percentage", s.LED, "2", "1").Output()
+			common.Log.Info("Get URL connected", "开灯")
+		} else {
+			exec.Command("/usr/test/led-hrtimer-close", s.LED).Output()
+			common.Log.Error("Get not connected", "关灯")
+		}
+	default:
+		log.Fatal("配置文件错误，类型仅支持 ping 或 status")
 	}
 }
