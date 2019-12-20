5653512f8a6d745ad11303a3cb1ce4ff3c563349
imnaive <nvhashida@icloud.com>
2019-12-20 13:33:10 +0800

    fix reboot

--- handlers/Reboot.go
+++ handlers/Reboot.go
@@ -2,16 +2,20 @@ package handlers
 
 import (
 	"os/exec"
-	"time"
 
+	"github.com/conthing/utils/common"
 	"github.com/gin-gonic/gin"
 )
 
 // Reboot 重启
 func Reboot(c *gin.Context) {
-	go func() {
-		time.Sleep(2 * time.Second)
-		exec.Command("reboot", "-f").Output() //初始化Cmd
-	}()
+
+	data, err := exec.Command("bash", "-c", "reboot -f").Output() //初始化Cmd
+	if err != nil {
+		common.Log.Error(err)
+		c.String(400, "关机失败", err)
+		return
+	}
+	common.Log.Debug(string(data))
 	c.String(200, "OK")
 }
