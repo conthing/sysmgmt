28a1c76d99cd82a8400fb4e6faba05d45d361670
imnaive <nvhashida@icloud.com>
2019-12-20 09:32:37 +0800

    fix: 放开路由

--- router/router.go
+++ router/router.go
@@ -15,15 +15,15 @@ func Service(cnf config.Config) {
 	router.Use(gin.Logger())
 	v1 := router.Group("/api/v1")
 	{
-		// v1.GET("/ping", handlers.Ping)                      // ✅
-		// v1.GET("/mac", handlers.GetMac)                     // ✅
-		// v1.PUT("/net", handlers.PutNet)                     // ✅
-		// v1.GET("/net/info", handlers.GetNetInfo)            // ✅
-		// v1.GET("/version-list", handlers.GetVersionList)    // ✅
-		// v1.PUT("/time", handlers.PutTime)                   // ✅
-		// v1.GET("/time/info", handlers.GetTimeInfo)          // ✅
-		// v1.PUT("/reboot", handlers.Reboot)                  // ❌
-		v1.POST("/update/file-upload", handlers.FileUpload) // ❌
+		v1.GET("/ping", handlers.Ping)                      // ✅
+		v1.GET("/mac", handlers.GetMac)                     // ✅
+		v1.PUT("/net", handlers.PutNet)                     // ✅
+		v1.GET("/net/info", handlers.GetNetInfo)            // ✅
+		v1.GET("/version-list", handlers.GetVersionList)    // ✅
+		v1.PUT("/time", handlers.PutTime)                   // ✅
+		v1.GET("/time/info", handlers.GetTimeInfo)          // ✅
+		v1.PUT("/reboot", handlers.Reboot)                  // ✅
+		v1.POST("/update/file-upload", handlers.FileUpload) // ✅
 
 	}
 	router.Run(fmt.Sprintf(":%d", cnf.Port))
