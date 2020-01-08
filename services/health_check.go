9e2078524bd8bf1c8c9156b770cff332b28629d7
imnaive <nvhashida@icloud.com>
2020-01-08 17:29:39 +0800

    增加检查 lpr

--- services/health_check.go
+++ services/health_check.go
@@ -3,6 +3,7 @@ package services
 import (
 	"fmt"
 	"net/http"
+	"os/exec"
 	"sysmgmt-next/config"
 	"time"
 
@@ -30,10 +31,27 @@ func HealthCheck() {
 	IsHealth = true
 }
 
+// CheckLpr 检查 lpr
+func CheckLpr() {
+	out, err := exec.Command("/bin/sh", "-c", "pgrep -l lpr").Output()
+	if err != nil {
+		common.Log.Error("CheckLpr 执行 shell 失败: ", err)
+	}
+	str := string(out)
+	common.Log.Info(str)
+	if str == "" {
+		_, err := exec.Command("cd /app/zap/lpr && LD_LIBRARY_PATH=. exec -a conthing-lpr ./lpr -d /app/log/lpr >/app/log/conthing-lpr.log 2>&1 &").Output()
+		if err != nil {
+			common.Log.Error("重启 lpr 失败", err)
+		}
+	}
+}
+
 // ScheduledHealthCheck 定时轮询任务
 func ScheduledHealthCheck() {
 	for {
 		HealthCheck()
+		CheckLpr()
 		time.Sleep(30 * time.Second)
 	}
 }
